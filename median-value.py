bf842adb810d308af72e3d713ed36cf7f4fff2fa
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-11-22 09:00:06 +0100

    making it a function

--- median-value.py
+++ median-value.py
@@ -1,24 +1,21 @@
 """
 Problem:
-How to find median age in a list without using a premade function and loop?
+How to find median value in a list without using a premade function and loop?
 
 Solution:
 """
-ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
-list_length       = len(ages)
-half_list_length  = int(list_length / 2)
+list1 = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+list2 = [22, 19, 24, 20, 25, 26, 24, 25, 24]
 
-if(list_length % 2):
-  print(ages[half_list_length])
-else:
-  print(ages[half_list_length-1])
-  print(ages[half_list_length])
-  
-"""
-alternatively:
-"""
-if(not (list_length % 2)):
-  print(ages[half_list_length-1])
-print(ages[half_list_length])
-  
+def find_median(x):
+    x_length = len(x)
+    x_length_half = int(x_length / 2)
+    
+    if(x_length % 2):
+        return x[x_length_half]
+    else: 
+        return x[x_length_half-1], x[x_length_half]
+        
+print(find_median(list1))
+print(find_median(list2))
